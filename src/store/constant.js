9a88cbf54b15a3a749d740328203a10f6b3f4717
priyanshupandey12 <priyanshupandat32@gmail.com>
2025-10-29 19:14:54 +0530

    add a line

--- src/store/constant.js
+++ src/store/constant.js
@@ -1,3 +1,4 @@
+console.log("🧭 VITE_BASE_URL =", import.meta.env.VITE_BASE_URL);
 export const BASE_URL = import.meta.env.VITE_BASE_URL;
 export const AGORA_APP_ID = import.meta.env.VITE_AGORA_APP_ID;
 export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
