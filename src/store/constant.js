7b0ef54eccdf67e76889e4eb7fcfd32e140c332e
priyanshupandey12 <priyanshupandat32@gmail.com>
2025-10-30 16:11:52 +0530

    update file

--- src/store/constant.js
+++ src/store/constant.js
@@ -1,9 +1,5 @@
 
-// export const BASE_URL ="https://api.devtalks.live/api/v1"
-// export const AGORA_APP_ID = import.meta.env.VITE_AGORA_APP_ID;
-// export const SOCKET_URL = "https://api.devtalks.live/"
-
-
 export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 export const AGORA_APP_ID = import.meta.env.VITE_AGORA_APP_ID;
+
